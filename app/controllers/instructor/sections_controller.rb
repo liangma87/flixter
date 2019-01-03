4c563bf4ee61ee802077034fd566fa33f3445b8c
liang <mliang3176@gmail.com>
2019-01-03 05:34:35 +0000

    added lessons

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -6,13 +6,13 @@ class Instructor::SectionsController < ApplicationController
 
   def create
     @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_param)
+    @section = @course.sections.create(section_params)
     redirect_to instructor_course_path(@course)
   end
 
   private
 
-  def section_param
+  def section_params
     params.require(:section).permit(:title)
   end
 end
