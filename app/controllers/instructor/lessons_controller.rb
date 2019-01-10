7b24575301d94385215281c048a636cafec18435
liang <mliang3176@gmail.com>
2019-01-10 05:24:41 +0000

    Added video uploading capability

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -7,8 +7,8 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def create
-    @lesson = @section.lessons.create(lesson_params)
-    redirect_to instructor_course_path(@section.course)
+    @lesson = current_section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(current_section.course)
   end
 
   private
@@ -25,7 +25,7 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle)
+    params.require(:lesson).permit(:title, :subtitle, :video)
   end
 
 end
