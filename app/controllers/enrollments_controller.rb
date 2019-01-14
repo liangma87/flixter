b1dc1a3162707e595ea49984a3beac16507fda5c
liang <mliang3176@gmail.com>
2019-01-14 01:51:21 +0000

    stripe CardError

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,33 +1,29 @@
 class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
+
   def create
+    if current_course.premium?
+      # Amount in cents
+      @amount = (current_course.cost * 100).to_i
 
-    begin
-      if current_course.premium?
-        # Amount in cents
-        @amount = (current_course.cost * 100).to_i
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
 
-        customer = Stripe::Customer.create(
-          email: params[:stripeEmail],
-          source: params[:stripeToken]
-        )
-
-        charge = Stripe::Charge.create(
-          customer: customer.id,
-          amount: @amount,
-          description: 'Flixter Premo Content',
-          currency: 'usd'
-        )
-      end
-    rescue 
-      Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to root_path
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Flixter Premo Content',
+        currency: 'usd'
+      )
     end
 
-    current_user.enrollments.create(course: current_course)
-    flash[:notice] = "You have been successfully enrolled to class '#{current_course.title}'"
-    redirect_to course_path(current_course)
+  current_user.enrollments.create(course: current_course)
+  redirect_to course_path(current_course)
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to root_path
   end
 
   private
