2d68d14fa2d9a2baa5ce7997143ca562580de288
liang <mliang3176@gmail.com>
2019-01-21 07:15:46 +0000

    fixed stale csrf issue

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def create
     @section = current_course.sections.create(section_params)
@@ -13,6 +14,13 @@ class Instructor::SectionsController < ApplicationController
   end
 
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+  
   def current_section
     @current_section ||= Section.find(params[:id])
   end
